e00c4972a28c49f9dce82be15ba4b8e9627d3aee
RichardPompeo <pompeorichard@gmail.com>
2026-04-09 19:46:45 -0300

    feat: setup express with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
