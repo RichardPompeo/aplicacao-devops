b17efa03affebeb49fb2e456e6b44e70b7bc6cef
RichardPompeo <pompeorichard@gmail.com>
2026-04-09 19:50:45 -0300

    feat: add GET /integrantes endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+const data = {
+  integrantes: [
+    { nome: 'Richard Pompeo' },
+    { nome: 'Gustavo Angelo' },
+    { nome: 'Henrique Ferraz' }
+  ]
+}
+
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname })
+})
+
+app.get('/integrantes', (req, res) => {
+  res.json(data)
 })
 
 app.listen(PORT, () => {
